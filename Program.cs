0be19e6e264dc19a1526f749627a7b8eb5a0f870
Robby Bourne <robbybourne1@gmail.com>
2017-09-22 13:29:13 -0400

    API works now

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WeatherApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://api.openweathermap.org/data/2.5/weather?zip=33609,us&appid=c8edcaf7bcbca5cf2dbc12521cdcc658";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var rawResponse = String.Empty;
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            var currentWeather = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+            Console.WriteLine($"Longitude: {currentWeather.coord.lon}");
+            Console.WriteLine($"Latitude: {currentWeather.coord.lat}");
+            Console.WriteLine($"City: {currentWeather.name}");
+            Console.ReadLine();
+        }
+    }
+}
